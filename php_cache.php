e9d23577579df89584d2a7cfe2880f76e2068e0b
刘子阳's ThinkPad <liuziyang_lzy@163.com>
2017-07-23 19:08:48 +0800

    php缓存字符串

---
+++ php_cache.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * redis 简单字符串缓存
+ */
+
+$redis = new \Redis();
+$redis->connect('127.0.0.1', 6379);
+
+//缓存数据
+$resis->set('cache-key', json_encode([
+	'data-list' => '这是一个缓存数据'
+	]), JSON_UNESCAPED_UNICODE);
+echo "字符串缓存成功\n\n";
+
+//从缓存获取数据
+$data = $redis->get('cache-key');
+echo "读取缓存数据为： \n";
+print_r(json_decode($data, true));
--- upvote_code.php
+++ upvote_code.php
@@ -1,8 +1,28 @@
 <?php
-    //连接本地的 Redis 服务
-   $redis = new Redis();
-   $redis->connect('127.0.0.1', 6379);
-   echo "Connection to server sucessfully";
-         //查看服务是否运行
-   echo "Server is running: " . $redis->ping();
-?>
+# <start id="upvote-code"/>
+define(ONE_WEEK_IN_SECONDS, 7 * 86400);				#A
+define(VOTE_SCORE, 432);							#A
+
+function article_vote($conn, $user, $article)
+{
+	$cutoff = time() = ONE_WEEK_IN_SECONDS;			#B
+	if ($conn->zscore('time:', $article) < $cutoff) {
+		return;
+	}
+}
+def article_vote(conn, user, article):
+    cutoff = time.time() - ONE_WEEK_IN_SECONDS      #B
+    if conn.zscore('time:', article) < cutoff:      #C
+        return
+
+    article_id = article.partition(':')[-1]         #D
+    if conn.sadd('voted:' + article_id, user):      #E
+        conn.zincrby('score:', article, VOTE_SCORE) #E
+        conn.hincrby(article, 'votes', 1)           #E
+# <end id="upvote-code"/>
+#A Prepare our constants
+#B Calculate the cutoff time for voting
+#C Check to see if the article can still be voted on (we could use the article HASH here, but scores are returned as floats so we don't have to cast it)
+#D Get the id portion from the article:id identifier
+#E If the user hasn't voted for this article before, increment the article score and vote count (note that our HINCRBY and ZINCRBY calls should be in a Redis transaction, but we don't introduce them until chapter 3 and 4, so ignore that for now)
+#END
