22bd2daa5e56f0801adda953f06cb3898dfbbc21
刘子阳's ThinkPad <liuziyang_lzy@163.com>
2017-07-24 10:57:44 +0800

    bug修复

--- php_cache.php
+++ php_cache.php
@@ -7,7 +7,7 @@ $redis = new Redis();
 $redis->connect('127.0.0.1', 6379);
 
 //缓存数据
-$resis->set('cache-key', json_encode([
+$redis->set('cache-key', json_encode([
 	'data-list' => '这是一个缓存数据'
 	]), JSON_UNESCAPED_UNICODE);
 echo "字符串缓存成功\n\n";
