7b9be758b11ae968440045b78da18911a45e233d
刘子阳's ThinkPad <liuziyang_lzy@163.com>
2017-07-24 15:07:54 +0800

    namespace

--- php_test_chapter01.php
+++ php_test_chapter01.php
@@ -1,10 +1,11 @@
 <?php
+namespace Foo;
+
 require 'php_redis_tool.php';
 
-$redis = "";
 $redis = redisConnect();
 
-$chapter = new FOO\Chapter01();
+$chapter = new Chapter01();
 $articleId = $chapter->postArticle($redis, 'liu', 'title', 'www.baidu.com');
 echo "We posted a new article with id: $articleId";
 echo "<br>";
